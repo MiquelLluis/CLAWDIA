44c9b2c269bfb2b6f668206f9cbf99ca6f733e42
Miquel Llorens <miquel.llorens@uv.es>
2022-10-03 16:07:56 +0200

    Add docstring

--- children.py
+++ children.py
@@ -5,6 +5,13 @@ import sklearn
 
 
 def omp_singlematch_batch(parents, dictionary, **kwargs):
+	"""TODO
+	Troba els indexs `i_atoms` amb els seus coeficients `c_atoms` dels àtoms
+	de `dictionary` més pareguts cada pare en `parents` gastant l'OMP.
+	Aquells pares per als que l'OMP no convergisca tindràn coeficient
+	`c_atoms[i] = 0`.
+
+	"""
 	# Ignore convergence warnings from sklearn's sparse_encode.
 	with warnings.catch_warnings():
 	    warnings.simplefilter('ignore', category=RuntimeWarning)
