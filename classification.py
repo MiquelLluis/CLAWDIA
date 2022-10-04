f4bf94b3db051e1f511a1eb38fc094ead70820ba
Miquel Llorens <miquel.llorens@uv.es>
2022-10-04 11:13:04 +0200

    Add optional custom label names

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def classificate_signal(parents, children, nc_val=-1):
+def classificate_signal(parents, children, labels=None, nc_val=-1):
     """TODO
 
     Quan tinga clar el procediment comentar-lo detalladament.
@@ -12,16 +12,16 @@ def classificate_signal(parents, children, nc_val=-1):
         Parent waveforms whose indices coincide to their respective morphological
         families (labels). Each parent waveform will have associated
         `n_parents` children.
-
     children: array_like, (samples, n_children, n_parents)
         Reconstructions associated with parent waveforms.
-    
+    labels: array_like, optional
+		Label names sorted. If None, will default to `range(n_parents)`.
     nc_val: int, -1 by default
         Value assigned to the non classified signals.
 
     RETURNS
     -------
-    index: int
+    label: int
         Index of the most fitting morphological family (label).
 
     """
@@ -37,22 +37,25 @@ def classificate_signal(parents, children, nc_val=-1):
         # If the denoising dictionary 'wf' was not able to reconstruct it
         # we assume impossible for the signal to belong to its morphology.
         if not p.any():
-            products[ip] = 1
+            discards[ip] = 1
             continue
         for ic in range(n_labels):
             c = children[:,ic,ip]
             products[ic] *= 1 - grawadile.estimators.ssim(p, c)
 
     losses = products + discards
-    index = np.argmin(losses)
+    label = np.argmin(losses)
 
     # If all losses are >= 1 then all are discarded.
-    if losses[index] >= 1:
+    if losses[label] >= 1:
         print('lost by all lossers')
-        index = nc_val
+        label = nc_val
     # If there are multiple minimums, mark it as not classified.
-    elif np.sum(losses == losses[index]) > 1:
+    elif np.sum(losses == losses[label]) > 1:
         print('lost by multiple minimums:', losses)
-        index = nc_val
+        label = nc_val
+
+    if labels is not None:
+    	label = labels[label]
     
-    return index
+    return label
