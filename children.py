d9344e2d14557949c5f796b288ed431f2eeb8f62
Miquel Llorens <miquel.llorens@uv.es>
2022-10-04 10:00:25 +0200

    Improve docstring

--- children.py
+++ children.py
@@ -76,6 +76,8 @@ def gen_index_children_batch(parents, dictionaries, dict_order=None, out=None, v
         Si s'introdueix, es gastarà per mapejar l'ordre en que s'introdueixen
         els children de cada diccionari dins `i_children`. Si no,
         s'introdueixen amb l'ordre que l'interpret de python esculla.
+    out: 4d-array (2, n_dictionaries, n_dictionaries, n_signals), optional
+        Eixida inplace opcional.
     **kwargs:
         Passat a la funció `sklearn.decomposition.sparse_encode`.
 
