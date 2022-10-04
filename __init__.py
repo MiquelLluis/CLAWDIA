dcf84d30468a14f66433d6744ead3f2fd7d5d4e1
Miquel Llorens <miquel.llorens@uv.es>
2022-10-04 09:51:19 +0200

    Add temporal header to init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-# Pipeline Claudia: Classification using Dictionary Algorithms
+""" Pipeline Claudia: Classification using Dictionary Algorithms
 
+TODO! Pipeline de classificació de senyals de detectors d'ones gravitacionals
+mitjançant algorismes de Dictionary Learning.
+
+"""
 from . import children
 from . import parents
 from . import util
