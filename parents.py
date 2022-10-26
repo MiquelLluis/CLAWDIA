dee9fcdaa19040a4deff5d45d67394a0e9dc3fee
Miquel Llorens <miquel.llorens@uv.es>
2022-10-26 12:01:52 +0200

    Add the option of automatic lambda reconstruction to the parent's methods

--- parents.py
+++ parents.py
@@ -5,16 +5,20 @@ from . import util
 
 def _gen_parents_inplace(signal, dictionaries, sc_lambda, out, **kwargs):
 	for ip, (key, dico) in enumerate(dictionaries.items()):
-		out[:,ip] = dico.reconstruct(signal, **kwargs)
+		out[:,ip] = dico.reconstruct(signal, sc_lambda, **kwargs)
 
 
-def gen_parents(signal, dictionaries, out=None, **kwargs):
+def _gen_parents_inplace_auto(signal, dictionaries, out, **kwargs):
+	for ip, (key, dico) in enumerate(dictionaries.items()):
+		out[:,ip] = dico.reconstruct_auto(signal, **kwargs)
+
+
+def gen_parents(signal, dictionaries, sc_lambda=None, out=None, **kwargs):
 	"""TODO
 	Genera els parents d'un senyal en un array numpy amb els diccionaris de
 	grawadile dins un dict. Si es dona el 'out' es clavaran els valors a
 	dintre en comptes de crear un nou array per als resultats. Els kwargs es
-	passen directament a cada diccionari de grawadile. Especificar que el
-	paràmetre 'sc_lambda' és obligatori!!!
+	passen directament a cada diccionari de grawadile.
 
 	"""
 	if out is None:
@@ -22,12 +26,15 @@ def gen_parents(signal, dictionaries, out=None, **kwargs):
 		n_dicts = len(dictionaries)
 		out = np.empty((l_window, n_dicts), order='F')
 	
-	_gen_parents_inplace(signal, dictionaries, out, **kwargs)
+	if sc_lambda is None:
+		_gen_parents_inplace(signal, dictionaries out, **kwargs)
+	else:
+		_gen_parents_inplace(signal, dictionaries, sc_lambda, out, **kwargs)
 
 	return out
 
 
-def gen_parents_batch(signals, dictionaries, sc_lambda, out=None, normalize=False, verbose=False, **kwargs):
+def gen_parents_batch(signals, dictionaries, sc_lambda=None, out=None, normalize=False, verbose=False, **kwargs):
 	"""TODO
 	Genera els parents d'un conjunt de senyals en un array (longitud, senyals)
 	de numpy amb els diccionaris de grawadile dins un dict. Si es dona el
@@ -45,13 +52,21 @@ def gen_parents_batch(signals, dictionaries, sc_lambda, out=None, normalize=Fals
 	for isi in range(n_signals):
 		if verbose:
 			print(f"Generating parents of #{isi}...")
-		_gen_parents_inplace(
-			signals[:,isi],
-			dictionaries,
-			sc_lambda,
-			out[...,isi],
-			**kwargs
-		)
+		if sc_lambda is None:
+			_gen_parents_inplace_auto(
+				signals[:,isi],
+				dictionaries,
+				out[...,isi],
+				**kwargs
+			)
+		else:
+			_gen_parents_inplace(
+				signals[:,isi],
+				dictionaries,
+				sc_lambda,
+				out[...,isi],
+				**kwargs
+			)
 
 	if normalize:
 		util.abs_normalize(out)
