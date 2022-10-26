cc81e02d937650ae19b5e54a604206b06bd35fd0
Miquel Llorens <miquel.llorens@uv.es>
2022-10-26 14:10:15 +0200

    Split up into two functions

--- parents.py
+++ parents.py
@@ -3,51 +3,78 @@ import numpy as np
 from . import util
 
 
-def _gen_parents_inplace(signal, dictionaries, sc_lambda, out, **kwargs):
+def _gen_parents_inplace(signal, dictionaries, sc_lambda, out, **kwargs_dico):
 	for ip, (key, dico) in enumerate(dictionaries.items()):
-		out[:,ip] = dico.reconstruct(signal, sc_lambda, **kwargs)
+		out[:,ip] = dico.reconstruct(signal, sc_lambda, **kwargs_dico)
 
 
-def _gen_parents_inplace_auto(signal, dictionaries, out, **kwargs):
+def _gen_parents_inplace_auto(signal, dictionaries, out, **kwargs_dico):
 	for ip, (key, dico) in enumerate(dictionaries.items()):
-		out[:,ip] = dico.reconstruct_auto(signal, **kwargs)
+		out[:,ip] = dico.reconstruct_auto(signal, **kwargs_dico)
 
 
-def gen_parents_batch(signals, dictionaries, sc_lambda=None, out=None, normalize=False, verbose=False, **kwargs):
+def gen_parents_batch(signals, dictionaries, sc_lambda, parents=None, normalize=False,
+					  verbose=False, **kwargs_dico):
 	"""TODO
+	
 	Genera els parents d'un conjunt de senyals en un array (longitud, senyals)
 	de numpy amb els diccionaris de grawadile dins un dict. Si es dona el
-	'out' (l_window, n_dicos, n_signals) es clavaran els valors a dintre en
-	comptes de crear un nou array per als resultats. Els kwargs es passen
-	directament a cada diccionari de grawadile.
+	'parents' (l_window, n_dicos, n_signals) es clavaran els valors a dintre en
+	comptes de crear un nou array per als resultats.
 
 	"""
 	l_window, n_signals = signals.shape
 	n_dicts = len(dictionaries)
 
-	if out is None:
-		out = np.empty((l_window, n_dicts, n_signals), order='F')
+	if parents is None:
+		parents = np.empty((l_window, n_dicts, n_signals), order='F')
 
 	for isi in range(n_signals):
 		if verbose:
 			print(f"Generating parents of #{isi}...")
-		if sc_lambda is None:
-			_gen_parents_inplace_auto(
-				signals[:,isi],
-				dictionaries,
-				out[...,isi],
-				**kwargs
-			)
-		else:
-			_gen_parents_inplace(
-				signals[:,isi],
-				dictionaries,
-				sc_lambda,
-				out[...,isi],
-				**kwargs
-			)
+		_gen_parents_inplace(
+			signals[:,isi],
+			dictionaries,
+			sc_lambda,
+			parents[...,isi],
+			**kwargs_dico
+		)
 
 	if normalize:
-		util.abs_normalize(out)
+		util.abs_normalize(parents)
 
-	return out
+	return parents
+
+
+def gen_parents_batch_auto(signals, dictionaries, parents=None, normalize=False, verbose=False,
+						   **kwargs_dico):
+	"""TODO
+	
+	Genera els parents d'un conjunt de senyals en un array (longitud, senyals)
+	de numpy amb els diccionaris de grawadile dins un dict. La lambda de
+	reconstrucció es calcula per bisecció tal que s'anul·le el màrge esquerre
+	especificat als `kwargs_dico`.
+	Si es dona el 'parents' (l_window, n_dicos, n_signals) es clavaran els 
+	valors a dintre en comptes de crear un nou array per als resultats.
+
+	"""
+	l_window, n_signals = signals.shape
+	n_dicts = len(dictionaries)
+
+	if parents is None:
+		parents = np.empty((l_window, n_dicts, n_signals), order='F')
+
+	for isi in range(n_signals):
+		if verbose:
+			print(f"Generating parents of #{isi}...")
+		_gen_parents_inplace_auto(
+			signals[:,isi],
+			dictionaries,
+			parents[...,isi],
+			**kwargs_dico
+		)
+
+	if normalize:
+		util.abs_normalize(parents)
+
+	return parents
