21ffe752849622941e8fe6c32de52e2c6dc8435b
Miquel Llorens <miquel.llorens@uv.es>
2022-10-28 10:57:16 +0200

    Implement load function

--- dictionaries.py
+++ dictionaries.py
@@ -14,10 +14,25 @@ except ImportError:
 
 
 #______________________________________________________________________________
-# Functions only tested with instances of DictionarySpams.
+# Load and save functions.
 
 def load(file):
-    pass
+    """TODO
+
+    Only tested with DictionarySpams!
+
+    """
+    dico_raw = dict(np.load(file))
+    # Initialize DictionarySpams instance
+    dico = DictionarySpams(dict_init=dico_raw.pop('dict_init'))
+    # Restore the state of the dictionary
+    for key, value in dico_raw.items():
+        # Restore all 0d-array to their former types
+        if d.ndim == 0:
+            dico_raw[key] = value.item()
+        setattr(dico, key, value)
+
+    return dico
 
 
 def save(file, dico):
