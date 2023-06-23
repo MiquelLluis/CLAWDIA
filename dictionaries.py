22c3984ea249b00190bec0e662b35e1f13d20836
Miquel Llorens <miquel.llorens@uv.es>
2023-06-23 11:01:38 +0200

    cleanup

--- dictionaries.py
+++ dictionaries.py
@@ -3,16 +3,10 @@ import warnings
 
 import numpy as np
 
-
 try:
     from ._dictionary_spams import DictionarySpams
 except ImportError:
     warnings.warn("spams-python not found, 'DictionarySpams' won't be available", ImportWarning)
-
-# try:
-#     from ._dictionary_sklearn import DictionarySklearn
-# except ImportError:
-#     warnings.warn("scikit-learn not installed, 'DictionarySklearn' won't be available", ImportWarning)
 
 
 #______________________________________________________________________________
