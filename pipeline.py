f329945f565e28e64d5b80d85bc54634e0bd3ec5
Miquel Llorens <miquel.llorens@uv.es>
2023-06-30 15:01:11 +0200

    add template for the future pipeline class

--- pipeline.py
+++ pipeline.py
@@ -7,33 +7,50 @@ import numpy as np
 from . import util
 
 
-def main():
-    with open('run-pipeline.yaml') as f:
-        cfg = yaml.safe_load(f)
+# Current steps to leave outside Claudia (or for future added steps):
+#------------------------------------------------------------------------------
 
-    # Load data:
-    # - Frame
-    # - gps times + labels
-    # - asd
-    # - dictionaries
+# INPUT
+# - Extract windows from frame
+# - Whiten(asd) + crop + norm
 
-    # Preprocessing:
-    # - Extract windows from frame
-    # - Whiten + crop + norm
-    # - Denoise + norm
-
-    # Classification:
-    # - Crop to fit atoms' length of the classification dictionary
-    # - Predict labels
-
-    # Write output to disk:
-    # - Whitened strains
-    # - Denoised strains
-    # - Prediction vector
-    # - F1 score
-    # - Confusion matrix
+# OUTPUT
+# - Denoised strains (optional)
+# - Prediction vector
+# - F1 score
+# - Confusion matrix
 
 
-if __name__ == '__main__':
-    main()
-    print(util.FINISHED)
+# Current steps to include:
+#------------------------------------------------------------------------------
+
+# INPUT:
+# - Strains: ndarray
+#       > Length = atoms' length!!!
+# - Dictionaries
+
+# Preprocessing:
+# - Denoise + norm
+
+# Classification:
+# - DictionaryLRSDL.predict labels
+
+#------------------------------------------------------------------------------
+
+
+
+class Pipeline:
+    def __init__(self):
+        # Load settings and dictionaries.
+        pass
+
+    def __call__(self, strains):
+        # Complete list of steps of the pipeline.
+        pass
+
+    def _preprocess(self):
+        # Can be extended by inheriting Pipeline.
+        pass
+
+    def _predict(self):
+        pass
