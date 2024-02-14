862b949a655aecbf2717b03c1bc2cf4e0877a040
Miquel Llorens <miquel.llorens@uv.es>
2024-02-14 15:04:31 +0100

    add estimator of the merger index position

--- clawdia/estimators.py
+++ clawdia/estimators.py
@@ -154,3 +154,14 @@ def snr(strain, *, psd, at, window=('tukey',0.5)):
     snr = np.sqrt(4 * at**2 * af * sum_)
 
     return snr
+
+
+def find_merger(h: np.ndarray) -> int:
+    """Estimate the index position of the merger in the given strain.
+    
+    This could be done with a better estimation model, like a gaussian in
+    the case of binary mergers. However for our current project this does not
+    make much difference.
+    
+    """
+    return np.argmax(np.abs(h))
