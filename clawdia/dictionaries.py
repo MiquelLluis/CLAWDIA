98dac609b2ff5e3b518a3b24cb738570c2eb8aeb
Miquel Llorens <miquel.llorens@uv.es>
2024-06-22 17:57:52 +0200

    add the ability to load SPAMS dictionaries from versions previous to v0.4
    Just compute the transposed of all dictionary components.

--- clawdia/dictionaries.py
+++ clawdia/dictionaries.py
@@ -17,15 +17,32 @@ def load(file):
             k=dico_raw.pop('k'), k0=dico_raw.pop('k0'), updateX_iters=dico_raw.pop('updateX_iters'),
             updateD_iters=dico_raw.pop('updateD_iters')
         )
+    
     else:
+        dict_init = dico_raw.pop('dict_init')
+        
+        # For backwards compatibility with versions previous to v0.4,
+        # transpose it from Fortran to C order.
+        if dict_init.flags.f_contiguous:
+            print('dict_init')
+            dict_init = dict_init.T
+        
         # Initialize DictionarySpams instance
-        dico = DictionarySpams(dict_init=dico_raw.pop('dict_init'))
+        dico = DictionarySpams(dict_init=dict_init)
+
 
     # Restore the state of the dictionary
     for key, value in dico_raw.items():
         # Restore all 0d-array to their former types
         if value.ndim == 0:
             value = value.item()
+        
+        # For backwards compatibility with versions previous to v0.4,
+        # transpose all dictionary components from Fortran to C order.
+        elif value.ndim == 2 and value.flags.f_contiguous:
+            print(key)
+            value = value.T
+
         setattr(dico, key, value)
 
     return dico
